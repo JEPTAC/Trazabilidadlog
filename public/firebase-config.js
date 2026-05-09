56d5b428388744b630f69d42cac6e44d41e75709
JEPTAC <j.perez@ei.com.co>
2026-05-09 18:08:16 -0500

    Add files via upload

---
+++ public/firebase-config.js
@@ -0,0 +1,19 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyTnCg0vcMo8AEHpVanQyovzAkVkoUXM8",
+  authDomain: "trazabilidadlog.firebaseapp.com",
+  projectId: "trazabilidadlog",
+  storageBucket: "trazabilidadlog.firebasestorage.app",
+  messagingSenderId: "456063912481",
+  appId: "1:456063912481:web:fa7bf72ae87ddf58d8f160",
+  measurementId: "G-YN690LB8B2"
+};
+
+export const appSettings = {
+  companyName: "Electroingeniería",
+  driveUploadUrl: "",
+  drivePublicFolderUrl: "",
+  localFallbackEnabled: true,
+  requireFirebaseForProduction: false,
+  idleRiskMinutes: 25,
+  criticalRiskMinutes: 75
+};
